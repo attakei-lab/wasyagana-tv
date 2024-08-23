177a4d4b27757a1ccf85e3bf616de0dc2419e55f
Kazuya Takei <myself@attakei.net>
2024-08-24 00:00:19 +0900

    feat: Dump video data as custom models

--- src/wasyagana_tv/cli/fetch_videos.py
+++ src/wasyagana_tv/cli/fetch_videos.py
@@ -1,31 +1,68 @@
 """動画情報の収集"""
-import json
+
 import time
+from datetime import timedelta
 from pathlib import Path
 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from pydantic import RootModel
+
+from .. import models
+
+VideoList = RootModel[list[models.Video]]
 
 PLAYLISTS = [
-    "UUReBAqqC-hc9d70gOftfitg",  # わしゃがなTV本体
-    "PLO-B0bLDsrNBmCVuaOmYETDLS9VsXRelq",  # 4GamerSP内 / わしゃがなTV（おまけ動画）
+    # わしゃがなTV本体
+    {
+        "id": "UUReBAqqC-hc9d70gOftfitg",
+        "delay": timedelta(),
+    },
+    # 4GamerSP内 / わしゃがなTV（おまけ動画）
+    {
+        "id": "PLO-B0bLDsrNBmCVuaOmYETDLS9VsXRelq",
+        "delay": timedelta(hours=1),
+    },
 ]
 
 
-def collect_videos(credentials, interval: int = 1):
+def collect_videos(credentials, interval: int = 1) -> VideoList:
     youtube_client = build("youtube", "v3", credentials=credentials)
     items = []
-    for idx, playlist_id in enumerate(PLAYLISTS):
+    for idx, playlist in enumerate(PLAYLISTS):
         params = {
             "part": "id,snippet,contentDetails",
-            "playlistId": playlist_id,
+            "playlistId": playlist["id"],
             "maxResults": 50,
         }
         cnt = 1
         while True:
             print(f"Call {idx+1}-{cnt}")
             resp = youtube_client.playlistItems().list(**params).execute()
-            items += resp["items"]
+            items += [
+                models.Video.model_validate(
+                    {
+                        "id": item["snippet"]["resourceId"]["videoId"],
+                        "title": item["snippet"]["title"],
+                        "description": item["snippet"]["description"],
+                        "channel": {
+                            "id": item["snippet"]["channelId"],
+                            "title": item["snippet"]["channelTitle"],
+                        },
+                        "thumbnails": {
+                            k: models.Thumbnail(
+                                size=k,
+                                url=v["url"],
+                                width=v["width"],
+                                height=v["height"],
+                            )
+                            for k, v in item["snippet"]["thumbnails"].items()
+                        },
+                        "published_at": item["snippet"]["publishedAt"],
+                    }
+                )
+                for item in resp["items"]
+            ]
             next_page_token = resp.get("nextPageToken", None)
             if not next_page_token:
                 break
@@ -36,7 +73,9 @@ def collect_videos(credentials, interval: int = 1):
 
 
 def main():
-    credentials = service_account.Credentials.from_service_account_file("service-account.json")
+    credentials = service_account.Credentials.from_service_account_file(
+        "service-account.json"
+    )
     videos = collect_videos(credentials)
     print(f"{len(videos)} videos")
-    Path("videos.json").write_text(json.dumps(videos, ensure_ascii=False, indent=2))
+    Path("videos.json").write_text(VideoList(videos).model_dump_json(indent=2))
---
+++ src/wasyagana_tv/models.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from typing import Literal
+
+from pydantic import BaseModel
+
+
+THUMBNAIL_SIZE = Literal["default", "medium", "high", "standard", "maxres"]
+
+
+class Channel(BaseModel):
+    id: str
+    title: str
+
+
+class Thumbnail(BaseModel):
+    size: str
+    url: str
+    width: int
+    height: int
+
+
+class Video(BaseModel):
+    id: str
+    title: str
+    description: str
+    channel: Channel
+    thumbnails: dict[str, Thumbnail]
+    published_at: datetime
